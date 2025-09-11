d1020b40906cb15f7c8d5caba83f3707d3cf5c0e
William do Carmo <william.carmo@gmail.com>
2025-09-11 15:54:16 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   appId: "1:1083746480355:web:a582c53d3c9a3cfa1ad5d6",
   storageBucket: "studio-4977373253-2de1c.firebasestorage.app",
   apiKey: "AIzaSyDJJ7MyIXW0-g4lxpKmDPpF1ivqPA9mHR0",
-  authDomain: "studio-4977373253-2de1c.firebaseapp.com",
+  authDomain: "app.foiumaideia.com",
   messagingSenderId: "1083746480355"
 };
 
